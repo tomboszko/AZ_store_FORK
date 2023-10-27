596d9d7d8bc0cd79a87da8ceb6bb3278d235ba5e
Thomas Gomree <gomreethomas@gmail.com>
2023-10-27 12:16:59 +0200

    Maj count shoppingcart

--- index.php
+++ index.php
@@ -103,12 +103,13 @@ function addToCart($product) {
     if ($productKey === false) {
         // Add new entry for new product
         $_SESSION['shoppingCart'][] = $product;
-        $count = count($_SESSION['shoppingCart']);
+        
     } else {
         // Update quantity if already in cart
         $_SESSION['shoppingCart'][$productKey]['quantity'] += $product['quantity'];
     }
 
+    count($_SESSION['shoppingCart']);
 }
 
 // If the add-to-cart form is submitted
